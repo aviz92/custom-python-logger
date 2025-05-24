697fe1d8f644c01065ff51a0f799d87b65edfffc
avizaguri-macbook <avi210892@gmail.com>
2025-05-24 14:14:36 +0300

    Update __all__ in __init__.py to include CustomLoggerAdapter; bump version to 1.0.8 in setup.py

--- custom_python_logger/__init__.py
+++ custom_python_logger/__init__.py
@@ -1,3 +1,10 @@
-from custom_python_logger.logger import get_logger, json_pretty_format, yaml_pretty_format
+from custom_python_logger.logger import (
+    get_logger, CustomLoggerAdapter, json_pretty_format, yaml_pretty_format
+)
 
-__all__ = ["get_logger", "json_pretty_format", "yaml_pretty_format"]
+__all__ = [
+    "get_logger",
+    "CustomLoggerAdapter",
+    "json_pretty_format",
+    "yaml_pretty_format"
+]
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-package_version = '1.0.7'
+package_version = '1.0.8'
 
 package_name = 'custom-python-logger'
 package_description = 'A custom logger with color support and additional features.'
